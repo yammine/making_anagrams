2cc38f9b5e9b1f265fccc5169525a59438b1f2e4
Christopher Yammine <christophery@mogo.ca>
2016-11-03 10:18:25 -0700

    Finds the number of deletions needed to make 2 given strings anagrams

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "math"
+)
+
+func main() {
+  scanner := bufio.NewScanner(os.Stdin);
+  scanner.Scan()
+  first := scanner.Text();
+  scanner.Scan()
+  second := scanner.Text();
+
+  var frequency [26]int;
+  var count int;
+
+  for i := 0; i < len(first); i++ {
+    letter := first[i];
+    frequency[letter - 'a'] += 1;
+  }
+
+  for j := 0; j < len(second); j++ {
+    letter := second[j];
+    frequency[letter - 'a'] -= 1;
+  }
+
+  for _, val := range(frequency) {
+    count += int(math.Abs(float64(val)));
+  }
+
+  fmt.Println(count);
+}
